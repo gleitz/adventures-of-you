133fa77aee15d885ed9e9893b0914ccc28884552
Benjamin Gleitzman <gleitz@mit.edu>
2020-06-08 18:00:12 -0700

    Admission

--- src/pages/de-lux-theater.tsx
+++ src/pages/de-lux-theater.tsx
@@ -16,7 +16,7 @@ const DeLuxPage = () => (
         It's time for a tasty and refreshing snack.
       </div>
       <div>
-Grab your buttery popcorn, Junior Mints, and Raisinets because we're headed to the <b>Shelter Island De Lux</b>, the Bay Area's most exclusive drive-in theater.
+        Grab your buttery popcorn, Junior Mints, and Raisinets because we're headed to the <b>Shelter Island De Lux Theater</b>, the Bay Area's most exclusive drive-in establishment.
       </div>
       <div>
         The fun begins <b>Saturday, June 13</b>, with coming attractions starting at 8PM.
@@ -25,7 +25,7 @@ Grab your buttery popcorn, Junior Mints, and Raisinets because we're headed to t
         It doesn't matter if you drive a hotrod corvette or an old jalopy, just bring your main squeeze and a working radio.
       </div>
       <div>
-        Want to find the address? Then you gotta play the game!
+        Want to find the address? Then you gotta play the game! The price of admission, matched by the De Lux Theater, will be a donation to an organization that supports racial justice.
       </div>
     </div>
     <a href="#">{generateFlatSpot("Saturday", DriveInKiss, DriveIn, "75%")}</a>
