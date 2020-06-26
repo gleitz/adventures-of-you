97234d48db7bc72b3635d0879606ba45db00b503
Benjamin Gleitzman <gleitz@mit.edu>
2020-06-26 12:01:50 -0700

    New island

--- src/pages/youre-invited.tsx
+++ src/pages/youre-invited.tsx
@@ -6,7 +6,7 @@ import { generateFlatSpot } from "../utils"
 
 import LoveCompy from "../images/love-compy.gif"
 import DogDance from "../images/dog-dance.gif"
-import SmileFace from "../images/smile-face.jpg"
+import ShelterIsland from "../images/shelter-island.gif"
 
 const IndexPage = () => (
   <Layout>
@@ -17,7 +17,7 @@ const IndexPage = () => (
       and under that was I born.”
       <br />
     </div>
-    <h1>Today!</h1>
+    <h1>Save the Date!</h1>
     <div className='padded'>
       <div>
       You're invited to <b>Pup's Pod Party: Shelter Island</b>, the Bay Area's most sparse dance party.
@@ -26,12 +26,12 @@ const IndexPage = () => (
         Want to find the address? Then you gotta play the game!
       </div>
       <div>
-        Game opens <b>today</b> at noon PST, with socially distant dance party from 2PM till sunset.
+        Game opens <b>Sunday, June 28</b> at noon PST, with socially distant dance party from <b>2PM</b> till sunset.
       </div>
       <div>
       </div>
     </div>
-    <a href="#">{generateFlatSpot("LET'S GO", DogDance, SmileFace, "75%")}</a>
+    <a href="#">{generateFlatSpot("Sunday", DogDance, ShelterIsland, "75%")}</a>
   </Layout>
 )
 
