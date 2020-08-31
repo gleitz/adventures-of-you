0a5eda007892596ab841f356e93f4ed1d4c3230c
Benjamin Gleitzman <gleitz@mit.edu>
2020-08-31 16:38:26 -0700

    Fix the link to the party

--- src/pages/mof-wednesday-worship-2020.tsx
+++ src/pages/mof-wednesday-worship-2020.tsx
@@ -26,7 +26,7 @@ class MoFWednesdayWorshipPage extends Component {
       <Layout>
         <SEO description="with the Monks of Funk" title="Wednesday Worship" />
         <div>
-          <a href="https://map.highfidelity.com/2EnLzUs798qmTxx6?mapJSON=https%3A%2F%2Fapi.jsonbin.io%2Fb%2F5f2195aec1edc4661760da97%2Flatest">
+          <a href="https://map.highfidelity.com/2EnLzUs798qmTxx6?mapJSON=https%3A%2F%2Fapi.jsonbin.io%2Fb%2F5f2195aec1edc4661760da97%2Flatest&x=11.02&y=21.42&o=-6.00">
             {generateFlatSpot("", PlayaSign, MonksLogo, "33%")}
           </a>
         </div>
