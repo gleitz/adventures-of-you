efbc0d5df6000cc812008297a2ac367c8243a281
Benjamin Gleitzman <gleitz@mit.edu>
2021-05-18 14:54:17 -0700

    Upgrade to Gatsby 3

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      fallback: {
+        "https": require.resolve("https-browserify"),
+        "http": require.resolve("stream-http")
+      }
+    }
+  })
+}
+
 // You can delete this file if you're not using it
