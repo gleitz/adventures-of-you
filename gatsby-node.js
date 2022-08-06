2acacc63447b2bdf2d6076787393e2e2a282d479
Benjamin Gleitzman <gleitz@mit.edu>
2022-08-06 01:58:01 -0700

    Fix CSS
    Don't incude CSS files on every page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,13 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      fallback: {
-      }
-    }
-  })
+
+// exports.onCreateWebpackConfig = ({ actions }) => {
+  // actions.setWebpackConfig({
+    // resolve: {
+      // fallback: {
+      // }
+    // }
+  // })
+// }
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === "build-javascript") {
+    const webpackConfig = getConfig()
+
+    delete webpackConfig.optimization.splitChunks.cacheGroups.styles
+
+    actions.replaceWebpackConfig(webpackConfig)
+  }
 }
 
 // You can delete this file if you're not using it
